e593b5f517ba97574128084ad37803fb89c98902
Roy Martin <roy@roy-martin.com>
2015-06-10 22:35:23 -0700

    Build clean-up to remove bower-component references. unused files and adjust editor configuration to reflect ModOp standard 4 spaces.

--- gulpfile.js
+++ gulpfile.js
@@ -12,98 +12,98 @@ var gulp = require('gulp'),
     reload = browserSync.reload;
 
 gulp.task('browserify', function() {
-  return browserify('./app/scripts/main.js')
-      .bundle()
+    return browserify('./app/scripts/main.js')
+        .bundle()
         .on('error', function(err) {
-          return notify().write(err);
+            return notify().write(err);
 
         })
         //Pass desired output filename to vinyl-source-stream
         .pipe(source('main.js'))
         .pipe(buffer())
         .pipe($.sourcemaps.init({
-          loadMaps: true
+            loadMaps: true
         }))
         .pipe($.sourcemaps.write('./'))
         // Start piping stream to tasks!
         .pipe(gulp.dest('.tmp/scripts'))
         .pipe(reload({
-          stream: true
+            stream: true
         }));
 });
 
 gulp.task('styles', function() {
-  gulp.src(['app/styles/main.scss', 'app/styles/vendor.scss'])
-      .pipe($.sourcemaps.init())
+    gulp.src(['app/styles/main.scss', 'app/styles/vendor.scss'])
+        .pipe($.sourcemaps.init())
         .pipe($.sass({
-          outputStyle: 'nested', // libsass doesn't support expanded yet
-          precision: 10,
-          includePaths: ['.'],
+            outputStyle: 'nested', // libsass doesn't support expanded yet
+            precision: 10,
+            includePaths: ['.'],
         }).on('error', function(err) {
-          return notify().write(err);
-          this.emit('end');
+            return notify().write(err);
+            this.emit('end');
         }))
         .pipe($.postcss([
             require('autoprefixer-core')({
-              browsers: ['last 1 version']
+                browsers: ['last 1 version']
             })
         ]))
         .pipe($.sourcemaps.write())
         .pipe(gulp.dest('.tmp/styles'))
         .pipe(reload({
-          stream: true
+            stream: true
         }));
 });
 
 
 gulp.task('sprites', function() {
-  return gulp.src('app/images/*.png')
-    .pipe(sprite({
-      name: 'sprite',
-      style: '_sprite.scss',
-      cssPath: '../images/sprite/',
-      processor: 'scss'
-    }))
-    .pipe($.if('*.png', gulp.dest('app/images/sprite'), gulp.dest('app/styles/scss/helper')));
+    return gulp.src('app/images/*.png')
+        .pipe(sprite({
+            name: 'sprite',
+            style: '_sprite.scss',
+            cssPath: '../images/sprite/',
+            processor: 'scss'
+        }))
+        .pipe($.if('*.png', gulp.dest('app/images/sprite'), gulp.dest('app/styles/scss/helper')));
 });
 
 gulp.task('html', ['styles'], function() {
-  var assets = $.useref.assets({
-    searchPath: ['app', '.']
-  });
+    var assets = $.useref.assets({
+        searchPath: ['app', '.']
+    });
 
-  return gulp.src('app/*.html')
-      .pipe(assets)
-      .pipe($.if('*.css', $.csso()))
-      .pipe(assets.restore())
-      .pipe($.useref())
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe($.if('*.css', $.csso()))
+        .pipe(assets.restore())
+        .pipe($.useref())
         .pipe($.if('*.html', $.minifyHtml({
-          conditionals: true,
-          loose: true
+            conditionals: true,
+            loose: true
         })))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('images',function() {
-  return gulp.src('app/images/**/*')
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*')
         .pipe($.cache($.imagemin({
-          progressive: true,
-          interlaced: true,
-          // don't remove IDs from SVGs, they are often used
-          // as hooks for embedding and styling
-          svgoPlugins: [{
-            cleanupIDs: false
-          }]
+            progressive: true,
+            interlaced: true,
+            // don't remove IDs from SVGs, they are often used
+            // as hooks for embedding and styling
+            svgoPlugins: [{
+                cleanupIDs: false
+            }]
         })))
         .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('extras', function() {
-  return gulp.src([
-      'app/*.*',
-      '!app/*.html'
+    return gulp.src([
+        'app/*.*',
+        '!app/*.html'
     ], {
-      dot: true
+        dot: true
     }).pipe(gulp.dest('dist'));
 });
 
@@ -111,55 +111,55 @@ gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 gulp.task('clean:js', require('del').bind(null, ['.tmp/scripts']));
 
 gulp.task('serve', ['styles', 'browserify', 'sprites'], function() {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: ['.tmp', 'app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: ['.tmp', 'app'],
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
 
-  // watch for changes
-  gulp.watch([
-      'app/*.html',
-      'app/images/**/*',
-      'app/scripts/**/*.js'
-  ]).on('change', reload);
+    // watch for changes
+    gulp.watch([
+        'app/*.html',
+        'app/images/**/*',
+        'app/scripts/**/*.js'
+    ]).on('change', reload);
 
-  gulp.watch('app/styles/**/*.scss', ['styles']);
-  gulp.watch(['app/scripts/**/*.js', 'app/scripts/**/**/*.hbs'], ['browserify']);
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch(['app/scripts/**/*.js', 'app/scripts/**/**/*.hbs'], ['browserify']);
 });
 
 gulp.task('serve:dist', ['build'], function() {
-  browserSync({
-    notify: false,
-    port: 9000,
-    server: {
-      baseDir: 'dist',
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
+    browserSync({
+        notify: false,
+        port: 9000,
+        server: {
+            baseDir: 'dist',
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
 });
 
-gulp.task('build', ['sprites', 'html', 'styles:build', 'images', 'extras','sprite:build', 'browserify:build'], function() {
-  return gulp.src('dist/**/*').pipe($.size({
-    title: 'build',
-    gzip: true
-  }));
+gulp.task('build', ['sprites', 'html', 'styles:build', 'images', 'extras', 'sprite:build', 'browserify:build'], function() {
+    return gulp.src('dist/**/*').pipe($.size({
+        title: 'build',
+        gzip: true
+    }));
 });
 
 gulp.task('browserify:build', ['clean:js'], function() {
-  return browserify('./app/scripts/main.js')
-      .bundle()
-      //Pass desired output filename to vinyl-source-stream
-      .pipe(source('main.js'))
-      // Start piping stream to tasks!
-      .pipe(gulp.dest('dist/scripts'));
+    return browserify('./app/scripts/main.js')
+        .bundle()
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('main.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('styles:build', ['styles'], function() {
@@ -181,20 +181,20 @@ gulp.task('styles:build', ['styles'], function() {
 });
 
 
-gulp.task('sprite:build',function() {
-  return gulp.src('app/images/sprite/*')
+gulp.task('sprite:build', function() {
+    return gulp.src('app/images/sprite/*')
         .pipe(gulp.dest('dist/images/sprite'));
 });
 
 gulp.task('styles:vendor', function() {
-  return gulp.src('dist/styles/vendor.css')
+    return gulp.src('dist/styles/vendor.css')
 
-       .pipe($.uncss({
-         html: ['dist/index.html']
-       }))
-       .pipe($.csso())
-       .pipe(gulp.dest('dist/styles'));
+    .pipe($.uncss({
+            html: ['dist/index.html']
+        }))
+        .pipe($.csso())
+        .pipe(gulp.dest('dist/styles'));
 })
 gulp.task('default', ['clean'], function() {
-  gulp.start('build');
+    gulp.start('build');
 });
