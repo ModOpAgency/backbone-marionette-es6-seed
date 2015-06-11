e2df277dcb4a0f21c5d35e1bf2cc002635486316
Roy Martin <roy@roy-martin.com>
2015-06-10 22:27:13 -0700

    remove unnecessary node dependency and increase baseline configuration for VMs to speed up standard deployments

--- Vagrantfile
+++ Vagrantfile
@@ -49,10 +49,10 @@ Vagrant.configure(2) do |config|
   #   vb.gui = true
   #
   #   # Customize the amount of memory on the VM:
-    vb.memory = "3072"
+    vb.memory = "4096"
 
     # number of processors
-    vb.cpus = 2
+    vb.cpus = 4
   end
   #
   # View the documentation for the provider you are using for more
--- vagrant-root-install.sh
+++ vagrant-root-install.sh
@@ -2,11 +2,8 @@ echo "##########################################################################
 echo "                     INSTALLING GLOBAL SCRIPTS                                  "
 echo "################################################################################"
 
-# Note the new setup script name for Node.js v0.12
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
-
 # Then install with:
 sudo apt-get update
 
 # Install build tools
-sudo apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap python-software-properties python build-essential libssl-dev git
+sudo apt-get install -y make g++ git curl vim libcairo2-dev libav-tools nfs-common portmap python-software-properties python build-essential libssl-dev
