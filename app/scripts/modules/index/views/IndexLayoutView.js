d97a3c58c6493196ad36dd72e593daac483bfb43
bryan huizi <bryan.huizi@modop.com>
2015-10-22 11:00:34 -0700

    [ BMES-14-marionette-behavior-example | adds behavior example ]

--- app/scripts/modules/index/views/IndexLayoutView.js
+++ app/scripts/modules/index/views/IndexLayoutView.js
@@ -1,8 +1,17 @@
 'use strict';
 
+import DestroyWarn from 'common/behaviors/DestroyWarn';
 import template from 'modules/index/templates/IndexLayoutView.hbs';
 
 export default Marionette.LayoutView.extend({
+    ui: {
+        "destroy": ".js-destroywarn-behavior"
+    },
+    behaviors: [{
+        behaviorClass: DestroyWarn,
+        message: 'You did something wrong.'
+    }],
+
   get template() {
     return template;
   },
