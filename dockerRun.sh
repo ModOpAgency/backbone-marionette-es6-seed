7c0c8c8708bf8fa1acf0583aba9b10d39eb41512
bryan huizi <bryan.huizi@modop.com>
2016-03-07 17:49:26 -0800

    [ updates docker script ]

--- dockerRun.sh
+++ dockerRun.sh
@@ -2,7 +2,9 @@
 # Default run arguments to start the docker build and mount the current directory into /data.
 # The default arguments will setup a symbolic link to node_modules, npm install, and gulp serve.
 # This can be overwritten by passing in an optional argument
+DOCKER_IMAGE=backbone-marionette-es6-node-5
 docker-machine start default
 eval "$(docker-machine env default)"
-docker rm -f backbone-marionette-es6 &> /dev/null
-docker run -it --rm -p 9000:9000 --name backbone-marionette-es6 -v $PWD:/data roymartin/backbone-marionette-es6 $1
+docker pull $DOCKER_IMAGE
+docker rm -f $DOCKER_IMAGE &> /dev/null
+docker run -it --rm -p 9000:9000 --name $DOCKER_IMAGE -v $PWD:/data roymartin/$DOCKER_IMAGE $1
