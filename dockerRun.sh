36a9e8e40a8b94889b0ceb0cddbbcf84e2156c31
Roy Martin <roy.martin@modop.com>
2016-04-16 16:55:53 -0700

    Updates to automate recovery from docker.

--- dockerRun.sh
+++ dockerRun.sh
@@ -6,20 +6,40 @@ DOCKER_IMAGE=backbone-marionette-es6-node-5
 COLOR_GREEN='tput setaf 2'
 COLOR_CLEAR='tput sgr0'
 
-echo "$($COLOR_GREEN)################################################################################$($COLOR_CLEAR)"
-echo "$($COLOR_GREEN)Starting docker and updating image$($COLOR_CLEAR)"
-echo "$($COLOR_GREEN)################################################################################$($COLOR_CLEAR)"
+function removeDockerMachine {
+    echo "$($COLOR_GREEN)Removing docker machine 'default' $($COLOR_CLEAR)"
+    docker-machine rm default -y
+}
 
+function createDockerMachine {
+    echo "$($COLOR_GREEN)Creating docker Machine 'default' $($COLOR_CLEAR)"
+    docker-machine create --driver virtualbox default
+}
+
+# Ensure that the docker machine exists
+if tmp="$(docker-machine ls | grep '^default')"; then
+    # Ensure that the docker machine hasn't been deleted
+    if tmp="$(docker-machine ls | grep '^default' | grep 'machine does not exist')"; then
+        # it exists needs to be re-created
+        removeDockerMachine
+        createDockerMachine
+    fi
+# Docker machine does not exist, create it
+else
+    createDockerMachine
+fi
+
+echo "$($COLOR_GREEN)Starting docker machine and updating image $($COLOR_CLEAR)"
 docker-machine start default
 eval "$(docker-machine env default)"
-# docker pull modop/$DOCKER_IMAGE
+docker pull modop/$DOCKER_IMAGE
+
 
 echo "$($COLOR_GREEN)################################################################################$($COLOR_CLEAR)"
 echo "$($COLOR_GREEN)Starting container $($COLOR_CLEAR)"
 echo "$($COLOR_GREEN)Running Command: npm install && npm start $($COLOR_CLEAR)"
 echo "$($COLOR_GREEN)Upon success open your browser to http://192.168.99.100:9000/$($COLOR_CLEAR)"
-echo "$($COLOR_GREEN)To access the command-line run with ./dockerRun.sh /bin/bash $($COLOR_CLEAR)"
+echo "$($COLOR_GREEN)To access the command-line run with docker-compose run --service-ports web bash $($COLOR_CLEAR)"
 echo "$($COLOR_GREEN)################################################################################$($COLOR_CLEAR)"
 
-docker rm -f $DOCKER_IMAGE &> /dev/null
-docker run -it --rm -p 9000:9000 --name $DOCKER_IMAGE -v $PWD:/data modop/$DOCKER_IMAGE $1
+docker-compose up
